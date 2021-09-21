c441ae3261ea78432378bc0d795381c80e7af588
ida-sofie <idasofib@stud.ntnu.no>
2021-09-21 12:40:08 +0200

    La til innhold i app-klasse

---
+++ Eliteserien/fxui/src/main/java/eliteserien/ui/EliteserienApp.java
@@ -0,0 +1,21 @@
+package main.java.eliteserien.ui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class EliteserienApp extends Application {
+
+  @Override
+  public void start(Stage stage) throws Exception {
+    Parent parent = FXMLLoader.load(getClass().getResource("Eliteserien.fxml"));
+    stage.setScene(new Scene(parent));
+    stage.show();
+  }
+
+  public static void main(String[] args) {
+    launch(EliteserienApp.class, args);
+  }
+}
