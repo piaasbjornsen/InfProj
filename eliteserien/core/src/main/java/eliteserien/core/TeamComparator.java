8ffbf2926fbcdcd52bc1005309bfcbc0d1d680a7
ida-sofie <idasofib@stud.ntnu.no>
2021-10-03 17:37:26 +0200

    tests for Table-class and Team-class

--- eliteserien/core/src/main/java/eliteserien/core/TeamComparator.java
+++ eliteserien/core/src/main/java/eliteserien/core/TeamComparator.java
@@ -7,13 +7,13 @@ import java.util.Comparator;
  * Unused class for now. Will be used in later steps of the project for TableTeam sorting. 
  */
 
-public class TeamComparator implements Comparator<SoccerTeam>, Serializable {
+public class TeamComparator implements Comparator<Team>, Serializable {
 
     public TeamComparator() {
 
     }
 
-    public int compare(SoccerTeam t1, SoccerTeam t2) {
+    public int compare(Team t1, Team t2) {
 // logic for comparing two teams based on points. Return positive number if team 1 has more points than team 2, and negative in opposite case.
         return 0;
     }
---
+++ eliteserien/fxui/src/test/java/eliteserien/ui/EliteserienAppTest.java
@@ -0,0 +1,7 @@
+package eliteserien.ui;
+
+import org.testfx.framework.junit5.ApplicationTest;
+
+public class EliteserienAppTest extends ApplicationTest {
+    
+}
