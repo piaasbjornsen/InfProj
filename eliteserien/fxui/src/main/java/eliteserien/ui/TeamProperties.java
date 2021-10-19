8cf6448585c680bbf3eb565f3f094fb16852f0de
Yngvil <yngvilks@stud.ntnu.no>
2021-10-19 13:15:24 +0200

    Add comments

--- eliteserien/fxui/src/main/java/eliteserien/ui/TeamProperties.java
+++ eliteserien/fxui/src/main/java/eliteserien/ui/TeamProperties.java
@@ -2,19 +2,43 @@ package eliteserien.ui;
 
 import javafx.beans.property.SimpleStringProperty;
 
+/**
+ * Class for properties to football teams
+ * Instead of these properties being a string (name) and integer (points), 
+ * They are now listenable objects, so the TableView is automatically updated when they change
+*/
+
 public class TeamProperties {
+
+    // Attributes: 
     private SimpleStringProperty name;
     private SimpleStringProperty points;
     
+    /**
+     * Constructor:
+     * Makes name and points into listenable objects
+     * @param name
+     * @param points
+    */
 
     TeamProperties (String name, String points) {
         this.name = new SimpleStringProperty(name);
         this.points = new SimpleStringProperty(points);
     }
 
+    /**
+     * getName is for TableView, so that the property Name gets its own coloumn
+     * @return name
+    */
+
     public String getName() {
         return name.get();
     }
+
+    /**
+     * getPoints is for TableView, so that the property Points gets its own coloumn
+     * @return points
+    */
 
     public String getPoints() {
         return points.get();
