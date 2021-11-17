f1e6154d98b1aedb6600a6831ff963d391b259c3
ida sofie <idasofib@stud.ntnu.no>
2021-11-17 14:51:28 +0100

    Edits in both controller classes, more private help methods and some structural changes

--- eliteserien/fxui/src/main/java/eliteserien/ui/EliteserienAppController.java
+++ eliteserien/fxui/src/main/java/eliteserien/ui/EliteserienAppController.java
@@ -24,12 +24,9 @@ import javafx.stage.Stage;
  * Controller class
  */
 
-public class EliteserienAppController{
+public class EliteserienAppController {
 
     // FXML attributes:
-
-    @FXML
-    String initialFileName; // Contains json-filename
 
     @FXML
     TableView<TeamProperties> tableView; // TableView for Elitserien table
@@ -47,43 +44,43 @@ public class EliteserienAppController{
     ChoiceBox<String> away; // Away team
 
     @FXML
-    TextField pointsH;     // Points to home team
+    TextField pointsH; // Points to home team
 
     @FXML
-    TextField pointsA;     // Points to away team
+    TextField pointsA; // Points to away team
 
     @FXML
-    Button saveButton;     // Save-match button
+    Button saveButton; // Save-match button
 
     @FXML
-    TextField message;     // Text field for error messages to user
+    TextField message; // Text field for error messages to user
 
     @FXML
-    TextField fileNameInput;
+    TextField fileNameInput; // filename input from user
 
     @FXML
-    Label tableName;
+    Label tableName; // name of the currently viewed table
 
     /**
-     * Attributes: 
-     * Tablepersistence object for reading and writing to json-file.
-     * Table object made by collecting data from the json-file.
-     * ObservableList object for tableView
-    */ 
+     * Attributes: Tablepersistence object for reading and writing to json-file.
+     * Table object made by collecting data from the json-file. ObservableList
+     * object for tableView
+     */
 
     private TablePersistence tablePersistence = new TablePersistence();
     private Table table;
-    private ObservableList<TeamProperties> teams = FXCollections.observableArrayList(); 
+    private ObservableList<TeamProperties> teams = FXCollections.observableArrayList();
+    private String initialFileName = "Eliteserien";
     private String fileName;
     private EditTableController editTableController;
 
-
-
     /**
-     * Returns a table object based on data collected from the json-file found in resource folder. 
+     * Returns a table object based on data collected from the json-file found in
+     * resource folder.
+     * 
      * @return initialTable
      * @throws "Could not read initial table" to terminal if IOException
-    */
+     */
 
     private Table getInitialTable() {
         Table initialTable = null;
@@ -96,14 +93,14 @@ public class EliteserienAppController{
     }
 
     /**
-     * Returns a table object based on data
-     * collected from the json-file in user.home folder. 
-     * If json-file is not found, the method will return 
-     * the table form initialTable-method.
+     * Returns a table object based on data collected from the json-file in
+     * user.home folder. If json-file is not found, the method will return the table
+     * form initialTable-method.
+     * 
      * @return getInitialTable() which returns initialTable
-    */
+     */
 
-    public Table getSavedTable() {
+    Table getSavedTable() {
         Table savedTable = null;
         try {
             savedTable = tablePersistence.loadSavedTable(fileName);
@@ -115,11 +112,12 @@ public class EliteserienAppController{
     }
 
     /**
-     * Saves the Table Object as json-file in user.home folder. 
+     * Saves the Table Object as json-file in user.home folder.
+     * 
      * @throws "Could not save Table" if IOException
-    */
+     */
 
-    public void saveTable() {
+    void saveTable() {
         try {
             tablePersistence.saveTable(table, fileName);
         } catch (IOException e) {
@@ -129,10 +127,11 @@ public class EliteserienAppController{
 
     /**
      * Adds all teams into a table object and returns this object
+     * 
      * @return Table object
-    */
+     */
 
-    public Table getTable() {
+    Table getTable() {
         Table getTable = new Table();
         for (Team team : table.getTeams()) {
             getTable.addTeams(team);
@@ -142,21 +141,19 @@ public class EliteserienAppController{
 
     /**
      * Sets table object
+     * 
      * @param table
-    */
+     */
 
-    public void setTable(Table table) {
+    void setTable(Table table) {
         this.table = table;
     }
-    
-    public void setFileName(String inputName) {
-        if (inputName.endsWith(".json")) {
-            this.fileName = inputName;
-        }
-        else {
-            this.fileName = inputName.concat(".json");
-        }
-    }
+
+        /**
+     * Sets tableText name
+     * 
+     * @param name name of table
+    */
 
     private void setTableName(String name) {
         tableName.setText(name);
@@ -164,9 +161,9 @@ public class EliteserienAppController{
 
     /**
      * Updates the observable list of teams to match the teams in table
-    */
+     */
 
-    private void updateTeamsList() {            
+    private void updateTeamsList() {
         teams.clear();
         for (Team team : table.getTeams()) {
             String name = team.getName();
@@ -177,131 +174,132 @@ public class EliteserienAppController{
 
     /**
      * Puts all teams in to the TableView area.
-    */
+     */
 
-    protected void setTableView() {
+    private void setTableView() {
         tableView.setItems(teams);
     }
 
     /**
-     * Updates team list
-     * Sets TableView with updated team list (for now)
-    */
-
-    protected void updateView() {
-        setTableName(fileName.substring(0, fileName.length() - 5));
-        updateTeamsList();
-        setChoices();
-        setTableView();
-    }
-
-    /**
-     * Sets choices for the choose-team boxes 
-     * This methos is only called on once in initialize 
-     * because the app does not support adding teams
-    */
-
-    private void setChoices(){            
-        home.getItems().clear();
-        away.getItems().clear();         
-        for (Team team : table.getTeams()){        
-           home.getItems().add(team.getName());
-           away.getItems().add(team.getName());
-        }
-    }
-
-    /**
-     * Checks if points added from the user is positive 
-     * If they are negative, it throws exception
-     * @param points
-     * @throws IllegalArgumentException
+     * Sets choices for the choose-team boxes This methos is only called on once in
+     * initialize because the app does not support adding teams
      */
-    private void validPoints(int points) throws IllegalArgumentException{   //check if points is positive
-        if(points < 0){         
-            throw new IllegalArgumentException();
+
+    private void setChoices() {
+        home.getItems().clear();
+        away.getItems().clear();
+        for (Team team : table.getTeams()) {
+            home.getItems().add(team.getName());
+            away.getItems().add(team.getName());
         }
     }
 
     /**
-     * When user pushes save button, this method:
-     * Clears old messages and input boxesand
-     * Checks for input-errors:
-        * Is points valid (positive)
-        * Are the chosen teams two different teams?
-     * Updates table
-     * Resets choice boxes and text fields
+     * Checks if points added from the user is valid
+     * Return false if pointText is empty, negative or is not integer.
+     * 
+     * @param points
+     * @throws NumberFormatException
+     * @return boolean value, true if pointstext is valid
+     */
+
+    boolean checkPointsText(TextField points) {
+        if (!points.getText().isEmpty()) { // If points field is not empty try parseInt (if empty: points will stay 0)
+            return false;
+        }
+        int pointsAsInt = 0;
+        try{
+            pointsAsInt = Integer.parseInt(points.getText());
+          } catch (NumberFormatException e) {
+            return false;
+          }
+        return pointsAsInt > 0;
+    }
+
+        
+             /**
+     * Add points to team object when user adds points
+     * 
+     * @param team
+     * @param points
+     */
+
+    private void addTeamPoints(String teamName, int points) {
+        for (Team team : table.getTeams()) { // Find teams and add points
+            if (team.getName().equals(teamName)) {
+                team.addPoints(points);
+            }
+        }
+    }
+
+        /**
+     * Checking if two teams are equal or fi either of them are null. 
+     * 
+     * @param homeTeam name as string
+     * @param awayTeam name as string
+     * @return boolean value, true if teams are different and not null.
     */
 
-    @FXML
-    public void handleSave(){
-        message.clear();                          // Clear old error message if any
-        String homeTeam = home.getValue();        // Extract input from choice boxes
-        String awayTeam = away.getValue();
-        int pointsHome = 0;                       // Set points 0
-        int pointsAway = 0;
-        try {
-            if(!pointsH.getText().isEmpty()) {          // If points field is not empty try parseInt (if empty: points will stay 0)
-                pointsHome = Integer.parseInt(pointsH.getText());
-            }
-            if(!pointsA.getText().isEmpty()){
-                pointsAway = Integer.parseInt(pointsA.getText());
-            }
-            validPoints(pointsHome);             // Check if points are valid (positive numbers only)
-            validPoints(pointsAway);
-        }
-        catch(IllegalArgumentException e){       // Exception thrown if points is not int or points<0
-            message.setText("Invalid points");   // Rrint error message to user
-            return;
-        }
-        if(homeTeam == null || awayTeam == null || homeTeam == awayTeam){    // Check if both teams are selected and different
-            message.setText("Choose two different teams");
-            return;
-        }
-        for (Team team : table.getTeams()){      // Find teams and add points
-            if(team.getName().equals(homeTeam)){
-                addTeamPoints(team, pointsHome);
-            }
-            else if(team.getName().equals(awayTeam)){
-                addTeamPoints(team, pointsAway);
-            }
-        }
-        home.setValue(null);                     // Reset choice boxes and text fields
+    private boolean checkTeams(String homeTeam, String awayTeam) {
+        return (homeTeam == null || awayTeam == null || homeTeam == awayTeam); 
+    }
+
+    /**
+     * sets home and away choiceboxes to null
+     * and clears points textFields
+     */
+
+    private void resetInputFields() {
+        home.setValue(null);
         away.setValue(null);
         pointsH.clear();
         pointsA.clear();
-        updateTeamsList();
-        updateView();
-        saveTable();
-    } 
+    }
 
     /**
-     * Add points to team object when user adds points
-     * @param team
-     * @param points
-    */
-
-    private void addTeamPoints(Team team, int points) {
-        team.addPoints(points);
-    }
-    
-    /** 
-     * use setTable method with input getInitialTable.
-     * The setTable method also calls the updateView method so 
-     * that the tableView attribute contains the initialTable.
-     * For testing: adding 1 point to each team and saving result 
-     * in json-file in user.home folder.
-    */
-
+     * When user pushes save button, this method: Clears old messages and input
+     * boxesand Checks for input-errors: Is points valid (positive) Are the chosen
+     * teams two different teams? Updates table Resets choice boxes and text fields
+     */
 
     @FXML
-    public void handleEdit(){
-        try{
-            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("EditTable.fxml"));            
+    void handleSave() {
+        message.clear(); // Clear old error message if any
+
+        if (!checkPointsText(pointsH) || !checkPointsText(pointsA)) {
+            message.setText("Invalid Points");
+            return;
+        }
+
+        if (!checkTeams(home.getValue(), away.getValue())) { // Check if both teams are selected and different
+            message.setText("Invalid teams");
+            return;
+        }
+        addTeamPoints(home.getValue(), Integer.parseInt(pointsH.getText()));
+        addTeamPoints(away.getValue(), Integer.parseInt(pointsA.getText()));
+        updateTeamsList();
+        resetInputFields();
+        updateView();
+        saveTable();
+    }
+
+    /**
+     * use setTable method with input getInitialTable. The setTable method also
+     * calls the updateView method so that the tableView attribute contains the
+     * initialTable. For testing: adding 1 point to each team and saving result in
+     * json-file in user.home folder.
+     */
+
+    @FXML
+    void handleEdit() {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("EditTable.fxml"));
             Parent root1 = (Parent) fxmlLoader.load();
             editTableController = fxmlLoader.getController();
             editTableController.setFileName(fileName);
             editTableController.setTable(table);
             editTableController.updateView();
+            editTableController.setAppController(this);
             Stage stage = new Stage();
             stage.setScene(new Scene(root1));
             stage.show();
@@ -310,14 +308,24 @@ public class EliteserienAppController{
         }
     }
 
-    @FXML
-    public void handleLoad(){
-        setTable(getSavedTable());
-        updateView();
+    /**
+     * setting field "filename" to input value
+     * 
+     * @param inputName
+     */
+
+    private void setFileName(String inputName) {
+        this.fileName = inputName.concat(".json");
     }
+
+    /**
+     * try opening file with input filename. if file is not found, a new file with
+     * input name is made with an empty table.
+     */
 
     @FXML
     void handleOpenFile() {
+        message.clear();
         Table saveTable = new Table();
         setFileName(fileNameInput.getText());
         try {
@@ -335,21 +343,44 @@ public class EliteserienAppController{
         updateView();
     }
 
+    /**
+     * Loads the saved table and updates view. User needs to press the load button
+     * to see changes made in edit window.
+     */
+
+    @FXML
+    void handleLoad() {
+        setTable(getSavedTable());
+        updateView();
+    }
+
+        /**
+     * Sets TableName as filename.
+     * Updates teams list.
+     * Sets team choice boxes.
+     * Sets tableView.
+     */
+
+    private void updateView() {
+        setTableName(fileName.substring(0, fileName.length() - 5));
+        updateTeamsList();
+        setChoices();
+        setTableView();
+    }
+
     @FXML
     public void initialize() {
-        if (fileName == null) {
-            setFileName(initialFileName);
-        }
+        setFileName(initialFileName);
         setTable(getSavedTable());
         updateTeamsList();
-        try{
-        setTableView();
-        teamsColumn.setCellValueFactory(new PropertyValueFactory<TeamProperties, String>("name"));
-        pointsColumn.setCellValueFactory(new PropertyValueFactory<TeamProperties, String>("points"));
-        updateView();
-        setChoices();
-        } catch(Exception e){
-
+        try {
+            setTableView();
+            teamsColumn.setCellValueFactory(new PropertyValueFactory<TeamProperties, String>("name"));
+            pointsColumn.setCellValueFactory(new PropertyValueFactory<TeamProperties, String>("points"));
+            updateView();
+            setChoices();
+        } catch (Exception e) {
+            System.err.println("Something went wrong.");
         }
     }
 }
