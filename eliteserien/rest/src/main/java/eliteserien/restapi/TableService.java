20037ca2dcec85c9634490b7a3403b9710645cd0
Yngvil <yngvilks@stud.ntnu.no>
2021-11-24 14:34:02 +0100

    Add comments in rest folder

--- eliteserien/rest/src/main/java/eliteserien/restapi/TableService.java
+++ eliteserien/rest/src/main/java/eliteserien/restapi/TableService.java
@@ -18,38 +18,53 @@ import java.io.IOException;
 
 
 /**
- * The top-level rest service for TodoModel.
- */
-@Path(TableService.ELITESERIEN_SERVICE_PATH)
-@Produces(MediaType.APPLICATION_JSON)
+ * Rest api for the web app.
+ * Framework for how to answer requests from client, and what requests to answer
+*/
+
+@Path(TableService.ELITESERIEN_SERVICE_PATH) // Tells the setup of the server where our service can be reached (through a eliteserien-prefix)
+@Produces(MediaType.APPLICATION_JSON)        // Tells the setup of the server that the return values from the methods in this class will be serialized with JSON back to the client
 public class TableService {
 
-  public static final String ELITESERIEN_SERVICE_PATH = "eliteserien";
+  public static final String ELITESERIEN_SERVICE_PATH = "eliteserien";           // The prefix that have to be used to ask for this spesific service
 
-  private static final Logger LOG = LoggerFactory.getLogger(TableService.class);
+  private static final Logger LOG = LoggerFactory.getLogger(TableService.class); // Logger to trigger log events for this class, to help trace out errors
+
+  /**
+   * The tableService needs a Table object and a TablePersistnece object
+   * The @Context annotation signalizes that the content/values to these objects do not come from this class,
+   * but needs to be injected/configured from an extern source
+   * (from rest server via TableConfig)
+   * So the methods in this class can expect these objects to be available for them 
+  */
+  
+  @Context
+  private Table table; 
 
   @Context
-  private Table table;
-
-  @Context
-  private TablePersistence tablePersistence;
+  private TablePersistence tablePersistence; 
 
   /**
    * The root resource, i.e. /eliteserien
-   *
-   * @return the Table
-   */
-  @GET
+   * 
+   * @return the injected Table
+  */
+
+  @GET // This method gets called when client sends a GET HTTP request
   public Table getTable() {
     System.out.println("getTable method tableService");
     LOG.debug("getTable: " + table);
     return table;
   }
 
-
+  /**
+   * Saves table
+   * Used in putTable and renameTable,
+   * @throws IOException if it is not able to save Table with saveTable method
+  */
 
   private void autoSaveTable() {
-    if (tablePersistence != null) {
+    if (tablePersistence != null) {  // Checks if there is a tablePeristicenc object avaialable to write to file
       try {
         tablePersistence.saveTable(table);
       } catch (IllegalStateException | IOException e) {
@@ -58,8 +73,15 @@ public class TableService {
     }
   }
 
-  @PUT
-  @Consumes(MediaType.APPLICATION_JSON)
+  /**
+   * Replaces or updates a Table.
+   * Synchronizes the server with the new changes
+   * @param tableArg the table with the new updates, or the table to replace current table
+   * @return true when done
+  */
+
+  @PUT // This method gets called when client sends a PUT HTTP request
+  @Consumes(MediaType.APPLICATION_JSON) // Tells the setup of the server that this method deserialize the parameter to make an object requested by the client
   public boolean putTable(Table tableArgs) {
     LOG.debug("putTable({})", tableArgs);
     for (Team team : tableArgs.getTeams()) {
@@ -70,10 +92,12 @@ public class TableService {
     return true;
   }
 
-    /**
-   * Emties the Table.
-   */
-  @DELETE
+  /**
+   * Deletes the teams in the table, so the Table is empty
+   * @return true when done
+  */
+
+  @DELETE // This method gets called when client sends a DELETE HTTP request
   public boolean emptyTable() {
     for (Team team : table.getTeams()) {
       table.deleteTeam(team);
--- eliteserien/rest/src/main/java/eliteserien/restserver/TableModuleObjectMapperProvider.java
+++ eliteserien/rest/src/main/java/eliteserien/restserver/TableModuleObjectMapperProvider.java
@@ -7,23 +7,21 @@ import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.ext.ContextResolver;
 import jakarta.ws.rs.ext.Provider;
 
-
 import eliteserien.json.TablePersistence;
 
 /**
  * Provides the Jackson module used for JSON serialization
- * ContextResolver<T> Contract for a provider that supplies context information to resource classes and other providers.
 */
 
-
-@Provider
-@Consumes(MediaType.APPLICATION_JSON)
-@Produces(MediaType.APPLICATION_JSON)
-public class TableModuleObjectMapperProvider implements ContextResolver<ObjectMapper> {
+@Provider 
+@Consumes(MediaType.APPLICATION_JSON) // Tells the setup of the server that the methods in the class deserialize the parameter to make an object requested by the client
+@Produces(MediaType.APPLICATION_JSON) // Tells the setup of the server that the return values from the methods in this class will be serialized with JSON back to the client
+public class TableModuleObjectMapperProvider implements ContextResolver<ObjectMapper> { // Implements from ContextResolver<T> To supply context information to resource classes and other providers.
     private final ObjectMapper objectMapper;  // ObjectMapper provides functionality for reading and writing JSON
 
     /**
      * Combines REST-api and core logic ???????????
+     * Customizes the objectmapper to the object to call createObjectMapper() in TablePErsistenceClass
     */
 
     public TableModuleObjectMapperProvider() {
