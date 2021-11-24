a340b686939edf7a8365a68cf5bb32538f51ecc8
ida sofie <idasofib@stud.ntnu.no>
2021-11-24 13:27:09 +0100

    Rest service tests
    Test methods added for integrationtests and rest modules. Used methods from todolist-project and edited to match methods in our porject

--- eliteserien/fxui/src/main/java/eliteserien/ui/RemoteApp.java
+++ eliteserien/fxui/src/main/java/eliteserien/ui/RemoteApp.java
@@ -7,6 +7,20 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class RemoteApp extends Application {
+    /**
+   * Helper method used by tests needing to run headless.
+   */
+  
+  public static void supportHeadless() {
+    if (Boolean.getBoolean("headless")) {
+      System.setProperty("testfx.robot", "glass");
+      System.setProperty("testfx.headless", "true");
+      System.setProperty("prism.order", "sw");
+      System.setProperty("prism.text", "t2k");
+      System.setProperty("java.awt.headless", "true");
+    }
+  }
+
     
     @Override
     public void start(Stage stage) throws Exception {
